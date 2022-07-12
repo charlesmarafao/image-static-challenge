9b98614a7e023d05353c956bd1c3978c998d8ea7
charlesmarafao <charles.marafao-ext@tuibrasil.com>
2022-07-11 23:44:37 -0300

    deploy

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,23 +1,25 @@
+var getLastCommitId = require('last-commit');
 import { Router } from 'express';
-import getLastCommitID from '../utils/getLastCommitID';
 import v1Router from './v1';
 import v2Router from './v2';
 import v3Router from './v3';
 
 const routes = Router();
 
-const lastCommitID = getLastCommitID();
+var lastCommitId: string;
+void (async function () {
+  lastCommitId = await getLastCommitId();
+})();
 
 [v1Router, v2Router, v3Router].forEach((router, i) => {
   i++;
   routes.use(`/api/v${i}`, router);
-
   routes.get(`/api/v${i}/healthcheck`, (r, res) => {
     const healthcheck = {
       uptime: process.uptime(),
       timestamp: Date.now(),
       apiVersion: i,
-      commitID: lastCommitID,
+      lastCommitId,
     };
 
     return res.status(200).json({ healthcheck });
